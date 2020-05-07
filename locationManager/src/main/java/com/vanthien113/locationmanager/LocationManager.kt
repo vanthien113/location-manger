dd2d415f5e7a8f563e27dce677be3e71a126f9ea
vanthien113 <vanthien113@gmail.com>
2020-05-07 16:58:10 +0700

    first commit

---
+++ locationManager/src/main/java/com/vanthien113/locationmanager/LocationManager.kt
@@ -0,0 +1,10 @@
+package com.vanthien113.locationmanager
+
+import android.content.Context
+import android.widget.Toast
+
+class LocationManager {
+    fun hello(context: Context) {
+        Toast.makeText(context, "Hello", Toast.LENGTH_SHORT).show()
+    }
+}
